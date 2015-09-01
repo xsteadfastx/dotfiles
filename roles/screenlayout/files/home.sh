6de3eee3a277621db6cf6c770cfa4825465bba80
Marvin Steadfast <marvin@xsteadfastx.org>
2015-09-01 08:15:03 +0200

    wallpaper for screenlayout

--- roles/screenlayout/files/home.sh
+++ roles/screenlayout/files/home.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 xrandr --output VGA-0 --off --output LVDS --mode 1600x900 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-0 --mode 1920x1080 --pos 1600x0 --rotate normal
-feh --no-xinerama --bg-tile ~/annex/Wallpaper/batman_owls.png
+feh --no-xinerama --bg-tile ~/.i3/wallpaper.png
