5baf98f3ee09661312707b22d82c4488328b041f
Marvin Steadfast <marvin@xsteadfastx.org>
2015-09-02 08:40:03 +0200

    new work screenlayout

--- roles/screenlayout/files/work.sh
+++ roles/screenlayout/files/work.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output VGA-0 --mode 1280x1024 --pos 1600x0 --rotate normal --output LVDS --mode 1600x900 --pos 0x124 --rotate normal --output DisplayPort-1 --off --output DisplayPort-0 --off
+xrandr --output VGA-0 --mode 1280x1024 --pos 0x0 --rotate normal --output LVDS --mode 1600x900 --pos 1280x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-0 --off
 feh --no-xinerama --bg-tile ~/.i3/wallpaper.png
