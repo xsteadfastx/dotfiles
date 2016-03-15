6ca0e5ddefc4276b1625af007be0d24900b2335e
Marvin Steadfast <marvin@xsteadfastx.org>
2016-03-15 09:38:54 +0100

    changed from vga to dvi for work setup

--- roles/screenlayout/files/work.sh
+++ roles/screenlayout/files/work.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output VGA-0 --mode 1280x1024 --pos 0x0 --rotate normal --output LVDS --mode 1600x900 --pos 1280x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-0 --off
+xrandr --output VGA-0 --off --output LVDS --mode 1600x900 --pos 1280x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-0 --mode 1280x1024 --pos 0x0 --rotate normal
 feh --no-xinerama --bg-tile ~/.i3/wallpaper.png
