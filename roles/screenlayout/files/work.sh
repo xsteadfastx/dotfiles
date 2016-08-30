a7a1040ec585c70bb5cebacd6c63e72f8c3c2211
Marvin Steadfast <marvin@xsteadfastx.org>
2016-08-30 14:07:14 +0200

    screenlayout: new screen setup

--- roles/screenlayout/files/work.sh
+++ roles/screenlayout/files/work.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-0 --off --output LVDS --mode 1600x900 --pos 1280x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-0 --mode 1280x1024 --pos 0x0 --rotate normal
-xsetroot -solid "#101010"
+xrandr --output VGA-2 --mode 1280x1024 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output VIRTUAL1 --off --output HDMI2 --off --output HDMI1 --off --output HDMI-3 --off --output DVI-I-1 --mode 1280x1024 --pos 1280x0 --rotate normal --output VGA1 --mode 1280x1024 --pos 2560x0 --rotate normal
