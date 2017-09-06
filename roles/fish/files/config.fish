f553e6da1579599261099ffbc3f0d575a112586a
Marvin Steadfast <marvin@xsteadfastx.org>
2017-09-06 12:39:20 +0200

    fish: add completions and stuff

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -12,3 +12,8 @@ if test -d ~/.pyenv/bin
     set PATH ~/.pyenv/bin $PATH
     status --is-interactive; and . (pyenv init -|psub)
 end
+
+# pipenv
+if test -f ~/.local/bin/pipenv
+    eval (env _PIPENV_COMPLETE=source-fish pipenv)
+end
