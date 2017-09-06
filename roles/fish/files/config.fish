cb342d753b0dfb2589b599d8c4223e05887ee23b
Marvin Steadfast <marvin@xsteadfastx.org>
2017-09-06 11:55:49 +0200

    from zsh to fish

---
+++ roles/fish/files/config.fish
@@ -0,0 +1,14 @@
+# aliases
+alias vim=nvim
+alias ll='ls -la'
+
+# path
+if test -d ~/.local/bin
+    set PATH ~/.local/bin $PATH
+end
+
+# pyenv
+if test -d ~/.pyenv/bin
+    set PATH ~/.pyenv/bin $PATH
+    status --is-interactive; and . (pyenv init -|psub)
+end
