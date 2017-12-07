da4fe2248978a2080d041359c1e03b5bef4da4b3
Marvin Steadfast <marvin@xsteadfastx.org>
2017-12-07 12:40:35 +0100

    i3: some i3pystatus work

--- roles/i3/files/i3status.py
+++ roles/i3/files/i3status.py
@@ -1,10 +1,21 @@
 from i3pystatus import Status
+from i3pystatus.weather import weathercom
 
 STATUS = Status()
 
 STATUS.register(
     'clock',
     format='%a %-d %b %X KW%V',
+)
+
+STATUS.register(
+    'weather',
+    format='[ {icon}] {current_temp}{temp_unit}[ {update_error}]',
+    colorize=True,
+    interval=900,
+    backend=weathercom.Weathercom(
+        location_code='GMNI2456:1:GM',
+    )
 )
 
 STATUS.register(
@@ -23,7 +34,7 @@ STATUS.register(
 
 STATUS.register(
     'battery',
-    format='{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}',
+    format='{status} {percentage:.2f}% {remaining:%E%hh:%Mm}',
     status={
         'DIS': '↓',
         'CHR': '↑',
@@ -47,13 +58,20 @@ STATUS.register(
 )
 
 STATUS.register(
+    'network',
+    interface='wlp3s0',
+    format_up='{essid} {quality:03.0f}% {v4cidr} {bytes_recv}KB/s',
+    format_down='',
+)
+
+STATUS.register(
     'openvpn',
     vpn_name='z10n'
 )
 
 STATUS.register(
     'openvpn',
-    vpn_name='tsm'
+    vpn_name='TSM'
 )
 
 STATUS.register(
