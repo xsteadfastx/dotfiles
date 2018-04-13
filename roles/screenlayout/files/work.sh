a28ce79f2e53ce781072d20e6841fcc71c001e4f
Marvin Steadfast <marvin@xsteadfastx.org>
2018-04-13 08:10:27 +0200

    screenlayout: disable laptop screen at work

--- roles/screenlayout/files/work.sh
+++ roles/screenlayout/files/work.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 3840x120 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP1-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP1-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
 feh --bg-fill ~/.i3/wallpaper.png
