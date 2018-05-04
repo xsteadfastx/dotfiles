bf6166a5217228866da90b0756217cc14c6eb3a9
Marvin Steadfast <marvin@xsteadfastx.org>
2018-05-04 08:25:12 +0200

    screenlayout: fixed work setup

--- roles/screenlayout/files/work.sh
+++ roles/screenlayout/files/work.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP1-1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP2 --off
+xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --mode 1920x1200 --pos 1920x0 --rotate normal
 feh --bg-fill ~/.i3/wallpaper.png
