85821a9d2370bd4eea1f188c70dc882811924862
Marvin Steadfast <marvin@xsteadfastx.org>
2018-08-22 09:13:34 +0200

    polybar: on all screens
    this starts polybar on all connected screens.

--- roles/polybar/files/launch.sh
+++ roles/polybar/files/launch.sh
@@ -6,7 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar top &
-
-echo "Bars launched..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
