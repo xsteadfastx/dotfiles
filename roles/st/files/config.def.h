2f834a3d6e85e5c7562b0a805d239d692b9f7a05
Marvin Steadfast <marvin@xsteadfastx.org>
2018-10-29 10:06:57 +0100

    changed font to fantasque

--- roles/st/files/config.def.h
+++ roles/st/files/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FuraMono Nerd Font Mono:pixelsize=14";
+static char *font = "FantasqueSansMono Nerd Font Mono:pixelsize=15";
 static int borderpx = 2;
 
 /*
