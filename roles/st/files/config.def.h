757f83edee7adfd68ab1b2e0992a7ed6fff2b9cf
Marvin Steadfast <marvin@xsteadfastx.org>
2019-01-14 09:23:52 +0100

    back to IBMPlex

--- roles/st/files/config.def.h
+++ roles/st/files/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "RobotoMono Nerd Font:pixelsize=14";
+static char *font = "IBMPlexMono Nerd Font Mono:pixelsize=14";
 static int borderpx = 2;
 
 /*
