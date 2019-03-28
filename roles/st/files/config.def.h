f016a1bb287c6d95ac023fc947cc373cfaa01f02
Marvin Steadfast <marvin@xsteadfastx.org>
2019-03-28 12:25:22 +0100

    Blex font

--- roles/st/files/config.def.h
+++ roles/st/files/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FantasqueSansMono Nerd Font:pixelsize=18:antialias=true:autohint=true";
+static char *font = "BlexMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
