a761abe57797e5e7b0356f433751f5b80652260a
Marvin Steadfast <marvin@xsteadfastx.org>
2019-05-02 09:25:11 +0200

    st: upgrade to 0.8.2

--- roles/st/files/config.def.h
+++ roles/st/files/config.def.h
@@ -84,31 +84,30 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-        /* 8 normal colors */
-        [0] = "#000000", /* black   */
-        [1] = "#ff5555", /* red     */
-        [2] = "#50fa7b", /* green   */
-        [3] = "#f1fa8c", /* yellow  */
-        [4] = "#bd93f9", /* blue    */
-        [5] = "#ff79c6", /* magenta */
-        [6] = "#8be9fd", /* cyan    */
-        [7] = "#bbbbbb", /* white   */
+  /* 8 normal colors */
+  [0] = "#000000", /* black   */
+  [1] = "#ff5555", /* red     */
+  [2] = "#50fa7b", /* green   */
+  [3] = "#f1fa8c", /* yellow  */
+  [4] = "#bd93f9", /* blue    */
+  [5] = "#ff79c6", /* magenta */
+  [6] = "#8be9fd", /* cyan    */
+  [7] = "#bbbbbb", /* white   */
 
-        /* 8 bright colors */
-        [8]  = "#44475a", /* black   */
-        [9]  = "#ff5555", /* red     */
-        [10] = "#50fa7b", /* green   */
-        [11] = "#f1fa8c", /* yellow  */
-        [12] = "#bd93f9", /* blue    */
-        [13] = "#ff79c6", /* magenta */
-        [14] = "#8be9fd", /* cyan    */
-        [15] = "#ffffff", /* white   */
+  /* 8 bright colors */
+  [8]  = "#44475a", /* black   */
+  [9]  = "#ff5555", /* red     */
+  [10] = "#50fa7b", /* green   */
+  [11] = "#f1fa8c", /* yellow  */
+  [12] = "#bd93f9", /* blue    */
+  [13] = "#ff79c6", /* magenta */
+  [14] = "#8be9fd", /* cyan    */
+  [15] = "#ffffff", /* white   */
 
-        /* special colors */
-        [256] = "#282a36", /* background */
-        [257] = "#f8f8f2", /* foreground */
+  /* special colors */
+  [256] = "#282a36", /* background */
+  [257] = "#f8f8f2", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
@@ -116,8 +115,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 257;
+unsigned int defaultcs = 257;
+unsigned int defaultrcs = 257;
 
 unsigned int defaultitalic = 7;
 unsigned int defaultunderline = 7;
@@ -177,8 +176,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 };
 
 /*
