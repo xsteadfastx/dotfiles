321fdd9216c74effcdfda42de409227596571eb4
Marvin Steadfast <marvin@xsteadfastx.org>
2019-05-23 08:24:29 +0200

    screenlayout: execute feh on work

--- roles/screenlayout/files/work.sh
+++ roles/screenlayout/files/work.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output eDP-1 --off --output DP-1-2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --mode 1920x1200 --pos 1920x0 --rotate normal
+feh --bg-fill ~/.i3/wallpaper.jpg
