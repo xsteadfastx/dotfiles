57242f470967cb2d342aa70d8e0dc925487f53b1
Marvin Steadfast <marvin@xsteadfastx.org>
2019-07-01 16:25:06 +0200

    Revert "fantasque mono"
    This reverts commit ce1655fa670f72bb4dc44dabdb8f718d36e97530.

--- roles/qutebrowser/files/config.py
+++ roles/qutebrowser/files/config.py
@@ -22,8 +22,6 @@ if uses_nouveau():
 config.bind(",m", "spawn /usr/bin/mpv {url}")
 # config.bind(",p", "spawn pocket-cli add -u {url}")
 config.bind(",p", "spawn --userscript qute-pass")
-# rebind for passthrough not being paste
-config.bind("<Ctrl-g>", "leave-mode", mode="passthrough")
 
 ### DRACULA ###########################
 import dracula.draw
@@ -32,7 +30,7 @@ dracula.draw.blood(
     c,
     {
         "spacing": {"vertical": 6, "horizontal": 8},
-        "font": {"family": "FantasqueSansMono Nerd Font", "size": 10},
+        "font": {"family": "FuraCode Nerd Font", "size": 10},
     },
 )
 ### /DRACULA ##########################
