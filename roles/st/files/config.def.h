0b16935be39ceeb80db236f66f8d74a600ddd354
Marvin Steadfast <marvin@xsteadfastx.org>
2019-08-22 10:23:21 +0200

    fantasque

--- roles/st/files/config.def.h
+++ roles/st/files/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font:pixelsize=18:antialias=true:autohint=true";
+static char *font = "FantasqueSansMono Nerd Font:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
