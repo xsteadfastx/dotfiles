95628876d141edae1835a1cbf1572d47abbdb78c
Marvin Steadfast <marvin@xsteadfastx.org>
2020-01-06 10:36:58 +0100

    cascadia font

--- .batcave/roles/st/files/config.def.h
+++ .batcave/roles/st/files/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "CascadianCode NF:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
