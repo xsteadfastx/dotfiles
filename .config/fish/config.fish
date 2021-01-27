7e576c7ae72f91bc88524b31cc12346e0741dc42
Marvin Steadfast <marvin@xsteadfastx.org>
2021-01-27 08:50:17 +0100

    fish: more grc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -85,20 +85,23 @@ end
 
 # grc
 if type -q grc
-  set -l execs cat cvs df diff dig gcc g++ ls ifconfig \
-               make mount mtr netstat ping ps tail traceroute \
-               wdiff
+  set -U grc_plugin_execs cat cvs df diff dig gcc g++ ls ifconfig \
+       make mount mtr netstat ping ps tail traceroute \
+       wdiff blkid du dnf docker docker-machine env id ip iostat \
+       last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
+       fdisk findmnt free semanage sar ss sysctl systemctl stat showmount \
+       tcpdump tune2fs vmstat w who
 
-  if set -q grc_plugin_execs
-    set execs $grc_plugin_execs
-  end
-
-  for executable in $execs
-    if type -q $executable
-      function $executable --inherit-variable executable --wraps=$executable
-        grc.wrap $executable $argv
+  for executable in $grc_plugin_execs
+      if type -q $executable
+          function $executable --inherit-variable executable --wraps=$executable
+              if isatty 1
+                  grc $executable $argv
+              else
+                  eval command $executable $argv
+              end
+          end
       end
-    end
   end
 end
 
