94d9422b989ca3839670037d72c79a91ba830484
Marvin Steadfast <marvin@xsteadfastx.org>
2021-02-17 11:33:47 +0100

    st: back to 0.8.3 to use ligatures patch

--- .batcave/roles/st/files/config.def.h
+++ .batcave/roles/st/files/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:pixelsize=17:antialias=true:autohint=true";
+static char *font = "FiraCode NerdFont:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -43,18 +43,9 @@ static unsigned int tripleclicktimeout = 600;
 /* alt screens */
 int allowaltscreen = 1;
 
-/* allow certain non-interactive (insecure) window operations such as:
-   setting the clipboard text */
-int allowwindowops = 0;
-
-/*
- * draw latency range in ms - from new content/keypress/etc until drawing.
- * within this range, st draws when content stops arriving (idle). mostly it's
- * near minlatency, but it waits longer for slow updates to avoid partial draw.
- * low minlatency will tear/flicker more, as it can "detect" idle too early.
- */
-static double minlatency = 8;
-static double maxlatency = 33;
+/* frames per second st should at maximum draw to the screen */
+static unsigned int xfps = 120;
+static unsigned int actionfps = 30;
 
 /*
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
@@ -95,39 +86,40 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
+        /* 8 normal colors */
+        [0] = "#000000", /* black   */
+        [1] = "#ff5555", /* red     */
+        [2] = "#50fa7b", /* green   */
+        [3] = "#f1fa8c", /* yellow  */
+        [4] = "#bd93f9", /* blue    */
+        [5] = "#ff79c6", /* magenta */
+        [6] = "#8be9fd", /* cyan    */
+        [7] = "#bbbbbb", /* white   */
 
-  /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
+        /* 8 bright colors */
+        [8]  = "#44475a", /* black   */
+        [9]  = "#ff5555", /* red     */
+        [10] = "#50fa7b", /* green   */
+        [11] = "#f1fa8c", /* yellow  */
+        [12] = "#bd93f9", /* blue    */
+        [13] = "#ff79c6", /* magenta */
+        [14] = "#8be9fd", /* cyan    */
+        [15] = "#ffffff", /* white   */
 
-  /* special colors */
-  [256] = "#282a36", /* background */
-  [257] = "#f8f8f2", /* foreground */
+        /* special colors */
+        [256] = "#282a36", /* background */
+        [257] = "#f8f8f2", /* foreground */
 };
+
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
-unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 257;
 
 unsigned int defaultitalic = 7;
 unsigned int defaultunderline = 7;
@@ -175,9 +167,7 @@ static uint forcemousemod = ShiftMask;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
