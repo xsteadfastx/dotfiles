d0d2c717d46f6757ca2e4561f9ab3c0584e8557c
Marvin Steadfast <marvin@xsteadfastx.org>
2021-05-26 08:43:37 +0200

    fish: pyenv path changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,124 +14,127 @@ set -e fish_user_paths
 
 # sbin
 if test -d /sbin
-  set fish_user_paths /sbin $fish_user_paths
+    set fish_user_paths /sbin $fish_user_paths
 end
 
 # poetry
 if test -d ~/.poetry
-  set fish_user_paths ~/.poetry/bin $fish_user_paths
+    set fish_user_paths ~/.poetry/bin $fish_user_paths
 end
 
 # go
 if type -q go
-  set -gx GOPATH ~/.local/share/go
-  set fish_user_paths ~/.local/share/go/bin $fish_user_paths
+    set -gx GOPATH ~/.local/share/go
+    set fish_user_paths ~/.local/share/go/bin $fish_user_paths
 end
 
 # gpg terminal agent
 if type -q gpg-agent
-  if [ (pgrep -x -u $USER gpg-agent) ]
-  else
-      gpg-connect-agent /bye >/dev/null 2>&1
-  end
-  set -x GPG_TTY (tty)
+    if [ (pgrep -x -u $USER gpg-agent) ]
+    else
+        gpg-connect-agent /bye >/dev/null 2>&1
+    end
+    set -x GPG_TTY (tty)
 end
 
 # gopass
 if type -q gopass
-  status --is-interactive; and gopass completion fish | source
+    status --is-interactive; and gopass completion fish | source
 end
 
 # nix
 if test -f ~/.nix-profile/bin/nix
-  set fish_user_paths ~/.nix-profile/bin $fish_user_paths
-  set -e LD_PRELOAD
-  if type -q bass
-    set -e NIX_PATH
-    bass source ~/.nix-profile/etc/profile.d/nix.sh
-  end
+    set fish_user_paths ~/.nix-profile/bin $fish_user_paths
+    set -e LD_PRELOAD
+    if type -q bass
+        set -e NIX_PATH
+        bass source ~/.nix-profile/etc/profile.d/nix.sh
+    end
 end
 
 # brew
 if test -f /home/linuxbrew/.linuxbrew/bin/brew
-  set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
-  set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
-  set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
-  set fish_user_paths /home/linuxbrew/.linuxbrew/bin $fish_user_paths
-  set fish_user_paths /home/linuxbrew/.linuxbrew/sbin $fish_user_paths
-  set fish_function_path /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d $fish_function_path
+    set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+    set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
+    set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
+    set fish_user_paths /home/linuxbrew/.linuxbrew/bin $fish_user_paths
+    set fish_user_paths /home/linuxbrew/.linuxbrew/sbin $fish_user_paths
+    set fish_function_path /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d $fish_function_path
 end
 
 # starship
 if type -q starship
-  starship init fish | source
+    starship init fish | source
 end
 
 # krew
 if test -d ~/.krew/bin
-  set fish_user_paths ~/.krew/bin $fish_user_paths
+    set fish_user_paths ~/.krew/bin $fish_user_paths
 end
 
 # pyenv
 if type -q pyenv
-  if not set -q POETRY_ACTIVE
-    status --is-interactive; and source (pyenv init -|psub)
-  end
+    if not set -q POETRY_ACTIVE
+        set -gx PYENV_ROOT $HOME/.pyenv
+        set fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+        status is-login; and pyenv init --path | source
+        pyenv init - | source
+    end
 end
 
 # direnv
 if type -q direnv
-  direnv hook fish | source
+    direnv hook fish | source
 end
 
 # grc
 if type -q grc
-  set -U grc_plugin_execs cat cvs df diff dig gcc g++ ls ifconfig \
-       make mount mtr netstat ping ps tail traceroute \
-       wdiff blkid du dnf docker docker-machine env id ip iostat \
-       last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
-       fdisk findmnt free semanage sar ss sysctl systemctl stat showmount \
-       tcpdump tune2fs vmstat w who
+    set -U grc_plugin_execs cat cvs df diff dig gcc g++ ls ifconfig \
+        make mount mtr netstat ping ps tail traceroute \
+        wdiff blkid du dnf docker docker-machine env id ip iostat \
+        last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
+        fdisk findmnt free semanage sar ss sysctl systemctl stat showmount \
+        tcpdump tune2fs vmstat w who
 
-  for executable in $grc_plugin_execs
-      if type -q $executable
-          function $executable --inherit-variable executable --wraps=$executable
-              if isatty 1
-                  grc $executable $argv
-              else
-                  eval command $executable $argv
-              end
-          end
-      end
-  end
+    for executable in $grc_plugin_execs
+        if type -q $executable
+            function $executable --inherit-variable executable --wraps=$executable
+                if isatty 1
+                    grc $executable $argv
+                else
+                    eval command $executable $argv
+                end
+            end
+        end
+    end
 end
 
 # home paths
 if test -d ~/.local/bin
-  set fish_user_paths ~/.local/bin $fish_user_paths
+    set fish_user_paths ~/.local/bin $fish_user_paths
 end
 
 set fish_user_paths ~/bin $fish_user_paths
 
 if test -d ~/bin/(uname -m)
-  set fish_user_paths ~/bin/(uname -m) $fish_user_paths
+    set fish_user_paths ~/bin/(uname -m) $fish_user_paths
 end
 
 if test -d ~/bin/(hostname)
-  set fish_user_paths ~/bin/(hostname) $fish_user_paths
+    set fish_user_paths ~/bin/(hostname) $fish_user_paths
 end
 
 # theme
 function fish_greeting
-  if type -q fortlit
-    fortlit
-  end
+    if type -q fortlit
+        fortlit
+    end
 end
 
 # bat
 if type -q bat
-  set -gx BAT_THEME Dracula
-  alias cat="bat"
+    set -gx BAT_THEME Dracula
+    alias cat="bat"
 end
 
 # fzf
@@ -139,7 +142,7 @@ set -gx FZF_DEFAULT_OPTS '--layout=reverse --color=dark --color=fg:-1,bg:-1,hl:#
 
 # vim
 if type -q nvim
-  abbr vim nvim
+    abbr vim nvim
 end
 
 # radio
@@ -148,9 +151,9 @@ abbr chillradio 'streamlink "https://www.youtube.com/watch?v=5qap5aO4i9A" 720p -
 
 # ls
 if type -q exa
-  abbr ll 'exa --git -la'
+    abbr ll 'exa --git -la'
 else
-  abbr ll 'ls -la'
+    abbr ll 'ls -la'
 end
 
 # search
@@ -160,23 +163,23 @@ abbr prev "fzf --preview 'bat --style=numbers --color=always {}'"
 
 # gping
 if type -q gping
-  abbr ping 'gping'
+    abbr ping gping
 end
 
 # kubernetes
-abbr k 'kubectl'
+abbr k kubectl
 abbr ks 'set -gx KUBECONFIG (fd -I -t f --exact-depth 1 . ~/.kube|fzf)'
 
 # git
-abbr g 'git'
+abbr g git
 abbr ga 'git add -A'
 abbr gc 'git commit'
 abbr gco 'git checkout'
 
 if type -q icdiff
-  abbr gd 'git icdiff'
+    abbr gd 'git icdiff'
 else
-  abbr gd 'git diff'
+    abbr gd 'git diff'
 end
 
 abbr gu 'git remote update'
