4703332a810703e44f683c9a1cde535706a688b5
Marvin Preuss <marvin@xsteadfastx.org>
2021-07-27 19:10:07 +0200

    fish: 2mp4 adds audiofallback

--- .config/fish/functions/2mp4.fish
+++ .config/fish/functions/2mp4.fish
@@ -5,6 +5,7 @@ function 2mp4
         --preset "Fast 1080p30" \
         --audio-lang-list "eng,deu" \
         --all-audio \
+        --aencoder copy --audio-fallback av_aac --ab 160 \
         --subtitle-lang-list "eng,deu" \
         --all-subtitles
 end
