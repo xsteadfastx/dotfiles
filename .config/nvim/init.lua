83ebb5288ed0932f195264c4f4b7c5bd47186dfd
Marvin Preuss <marvin@xsteadfastx.org>
2021-11-10 13:37:25 +0100

    feat(vim): adds integration build tag for golangci-lint linter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -39,6 +39,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
         fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
+cmd [[packadd packer.nvim]]
 require("packer").startup(
     {
         function(use)
@@ -202,8 +203,8 @@ require("packer").startup(
                     lint.linters.golangcilint.args = {
                         "run",
                         "--enable-all",
-                        "--disable",
-                        "godox,tagliatelle,exhaustivestruct,varnamelen",
+                        "--disable=godox,tagliatelle,exhaustivestruct,varnamelen",
+                        "--build-tags=integration",
                         "--out-format",
                         "json"
                     }
