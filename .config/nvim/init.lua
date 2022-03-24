ff2da8da91883a2a17d1972e566483fa9fb4d213
Marvin Preuss <marvin@xsteadfastx.org>
2022-03-24 15:17:45 +0100

    refactor(vim): new goimports function

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,37 +29,18 @@ local function nvim_create_augroups(definitions)
   end
 end
 
-function Goimports(timeout_ms) -- from https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
-  local context = {only = {"source.organizeImports"}}
-  vim.validate {context = {context, "t", true}}
-
+function OrgImports(wait_ms) -- from https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
   local params = vim.lsp.util.make_range_params()
-  params.context = context
-
-  -- See the implementation of the textDocument/codeAction callback
-  -- (lua/vim/lsp/handler.lua) for how to do this properly.
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-  if not result or next(result) == nil then
-    return
-  end
-  local actions = result[1].result
-  if not actions then
-    return
-  end
-  local action = actions[1]
-
-  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-  -- is a CodeAction, it can have either an edit, a command or both. Edits
-  -- should be executed first.
-  if action.edit or type(action.command) == "table" then
-    if action.edit then
-      vim.lsp.util.apply_workspace_edit(action.edit)
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit)
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
     end
-    if type(action.command) == "table" then
-      vim.lsp.buf.execute_command(action.command)
-    end
-  else
-    vim.lsp.buf.execute_command(action)
   end
 end
 
@@ -639,7 +620,7 @@ nvim_create_augroups({lint = {{"BufWritePost", "<buffer>", "lua require('lint').
 -- FORMAT ---------------------------------------
 nvim_create_augroups({format = {{"BufWritePost", "*", "FormatWrite"}}})
 nvim_create_augroups({lsp_format = {{"BufWritePre", "*", "lua vim.lsp.buf.formatting_seq_sync()"}}})
-nvim_create_augroups({goimports = {{"BufWritePre", "*.go", "lua Goimports(1000)"}}})
+nvim_create_augroups({org_imports = {{"BufWritePre", "*.go", "lua OrgImports(1000)"}}})
 
 -- GIST -----------------------------------------
 g["gist_post_private"] = 1
