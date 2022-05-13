8f487f17b00a8ee682c6bd6ede74392f34e3cd97
Marvin Preuss <marvin@xsteadfastx.org>
2022-05-13 10:06:10 +0200

    feat(vim): change nvim-lsp-installer config
    this should fix this deprecation: https://github.com/williamboman/nvim-lsp-installer/discussions/636

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -119,32 +119,42 @@ require("packer").startup(
 
     use {
       "williamboman/nvim-lsp-installer",
+      requires = {
+        "hrsh7th/nvim-cmp"
+      },
       {
         "neovim/nvim-lspconfig",
         config = function()
-          local lsp_installer = require("nvim-lsp-installer")
+          local lspinstaller = require("nvim-lsp-installer")
+          lspinstaller.setup {}
 
-          lsp_installer.on_server_ready(
-            function(server)
-            local opts = {}
+          local lspconfig = require("lspconfig")
 
-            if server.name == "gopls" then
-              opts.init_options = { buildFlags = { "-tags=integration,tools" }, gofumpt = true }
-            end
+          -- individuell lsp configs
+          lspconfig.gopls.setup {
+            init_options = {
+              buildFlags = { "-tags=integration,tools" },
+              gofumpt = true
+            }
+          }
 
-            if server.name == "sumneko_lua" then
-              opts.settings = {
-                Lua = {
-                  diagnostics = {
-                    globals = { "vim" }
-                  }
+          lspconfig.sumneko_lua.setup {
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" }
                 }
               }
-            end
+            }
+          }
 
-            server:setup(opts)
+          -- setup the rest
+          for _, server in ipairs(lspinstaller.get_installed_servers()) do
+            if server.name ~= "sumneko_lua" and server.name ~= "gopls" then
+              lspconfig[server.name].setup {}
+            end
           end
-          )
+
           vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
             vim.lsp.handlers.hover,
             {
@@ -158,6 +168,9 @@ require("packer").startup(
             border = "single"
           }
           )
+
+          -- completions
+          require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
         end
       }
     }
@@ -183,7 +196,6 @@ require("packer").startup(
     use {
       "hrsh7th/nvim-cmp",
       requires = {
-        { "neovim/nvim-lspconfig" },
         { "hrsh7th/cmp-nvim-lsp" },
         { "hrsh7th/cmp-buffer" },
         { "hrsh7th/cmp-path" },
@@ -234,9 +246,6 @@ require("packer").startup(
           )
         }
         )
-
-        -- lspconfig
-        require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
       end
     }
 
