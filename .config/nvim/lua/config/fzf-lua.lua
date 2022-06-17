1882dcb893801c6c7a73e60108fb9525ba1e930f
Marvin Preuss <marvin@xsteadfastx.org>
2022-06-17 12:51:35 +0200

    feat(vim): sets fd options in fzf for grepping in hidden files

--- .config/nvim/lua/config/fzf-lua.lua
+++ .config/nvim/lua/config/fzf-lua.lua
@@ -4,5 +4,8 @@ require("fzf-lua").setup {
 	},
 	files = {
 		fd_opts = "--color=always --type f --hidden --no-ignore --follow --exclude .git"
+	},
+	grep = {
+		rg_opts = "--no-ignore-vcs --hidden"
 	}
 }
