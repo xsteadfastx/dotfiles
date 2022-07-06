26ca2f2db9958d3a3a1ced44bc8d473b9ceb78c2
Marvin Preuss <marvin@xsteadfastx.org>
2022-07-06 12:18:41 +0200

    feat(vim): adds javascript formatter

--- .config/nvim/lua/config/formatter.lua
+++ .config/nvim/lua/config/formatter.lua
@@ -100,6 +100,19 @@ require("formatter").setup {
 					stdin = true
 				}
 			end
+		},
+		javascript = {
+			function()
+				return {
+					exe = "prettier",
+					args = {
+						"--stdin-filepath",
+						vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+						"--single-quote"
+					},
+					stdin = true
+				}
+			end
 		}
 	}
 }
