739e97f0ada976ff792fcf557c5f14dfdac18467
Marvin Preuss <marvin@xsteadfastx.org>
2022-08-19 09:07:18 +0200

    feat(vim): adds markdownlint

--- .config/nvim/lua/config/nvim-lint.lua
+++ .config/nvim/lua/config/nvim-lint.lua
@@ -4,6 +4,7 @@ lint.linters_by_ft = {
 	sh = { "shellcheck" },
 	ansible = { "ansible_lint" },
 	dockerfile = { "hadolint" },
+	markdown = { "markdownlint" }
 }
 
 require("helpers").create_augroups({ format = { { "BufWritePost", "*", "lua require('lint').try_lint()" } } })
