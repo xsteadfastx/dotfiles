a3f0030c9c541d43ab4fefc1fac49a6cf0327013
Marvin Preuss <marvin@xsteadfastx.org>
2022-08-29 17:54:26 +0200

    feat(vim): dapui autoopen

--- .config/nvim/lua/xsfx/packer.lua
+++ .config/nvim/lua/xsfx/packer.lua
@@ -218,6 +218,21 @@ require("packer").startup({
 			requires = { "mfussenegger/nvim-dap", "rcarriga/nvim-dap-ui" },
 			config = function()
 				require("dap-go").setup()
+
+				local dap = require("dap")
+				local dapui = require("dapui")
+				dapui.setup()
+
+				-- auto starting dapui
+				dap.listeners.after.event_initialized["dapui_config"] = function()
+					dapui.open()
+				end
+				dap.listeners.before.event_terminated["dapui_config"] = function()
+					dapui.close()
+				end
+				dap.listeners.before.event_exited["dapui_config"] = function()
+					dapui.close()
+				end
 			end,
 		})
 		use({
