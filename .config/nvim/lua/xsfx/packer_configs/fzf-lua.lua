e89c64a176e6bdcd8e6cccda4af0c9595479bd35
Marvin Preuss <marvin@xsteadfastx.org>
2022-08-30 10:43:01 +0200

    feat(vim): fd and rg exclude vendor and caches

--- .config/nvim/lua/xsfx/packer_configs/fzf-lua.lua
+++ .config/nvim/lua/xsfx/packer_configs/fzf-lua.lua
@@ -3,9 +3,9 @@ require("fzf-lua").setup({
 		fullscreen = true,
 	},
 	files = {
-		fd_opts = "--color=always --type f --hidden --no-ignore --follow --exclude .git",
+		fd_opts = "--color=always --type f --hidden --no-ignore --follow --exclude .git --exclude vendor --exclude .cache",
 	},
 	grep = {
-		rg_opts = "--no-ignore-vcs --hidden --column --line-number --no-heading --color=always --smart-case --max-columns=512",
+		rg_opts = "--no-ignore-vcs --hidden --column --line-number --no-heading --color=always --smart-case --max-columns=512 -g '!vendor/*' -g '!.git/*'",
 	},
 })
