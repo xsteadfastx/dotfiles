894e1633aea2174559b50712f7d5cac197aed222
Marvin Preuss <marvin@xsteadfastx.org>
2022-09-01 15:06:02 +0200

    feat(vim): better ttd and ife snippet

--- .config/nvim/lua/xsfx/packer_configs/luasnip-cfg.lua
+++ .config/nvim/lua/xsfx/packer_configs/luasnip-cfg.lua
@@ -1,12 +1,63 @@
 local ls = require("luasnip")
+local s = ls.snippet
+-- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
+-- local t = ls.text_node
+local i = ls.insert_node
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require("luasnip.util.events")
+-- local ai = require("luasnip.nodes.absolute_indexer")
+local fmt = require("luasnip.extras.fmt").fmt
+-- local extras = require("luasnip.extras")
+-- local m = extras.m
+-- local l = extras.l
+-- local rep = extras.rep
+-- local postfix = require("luasnip.extras.postfix").postfix
 
 ls.setup({})
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
 ls.add_snippets("go", {
-	ls.parser.parse_snippet(
-		"tdd",
-		'func Test$1(t *testing.T) {\n\ttables := []struct{\n\t\tname\tstring\n\t\t$2\n\t}{\n\t\t{\n\t\t\tname: "$3",\n\t\t\t$4\n\t\t},\n\t}\n\tfor _, tt := range tables {\n\t\ttt := tt\n\t\tt.Run(tt.name, func(t *testing.T) {\n\t\t\t$0\n\t\t})\n\t}\n}'
+	-- test tables
+	s(
+		"tddt",
+		fmt(
+			[[
+			func Test<>(t *testing.T) {
+				tables := []struct{
+					name string
+				}{
+					{
+						"00",
+					},
+				}
+
+				for _, tt := range tables {
+					tt := tt
+
+					t.Run(tt.name, func(t *testing.T){})
+				}
+			}
+		]],
+			{ i(1) },
+			{ delimiters = "<>" }
+		)
+	),
+	-- simple if err
+	s(
+		"ife",
+		fmt(
+			[[
+			if err != nil {
+					return <2>fmt.Errorf("<1>: %w", err)
+			}
+		]],
+			{ i(1), i(2) },
+			{ delimiters = "<>" }
+		)
 	),
 })
