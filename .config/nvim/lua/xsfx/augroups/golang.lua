68457eeff840574affd1b3c119d79c906970c54c
Marvin Preuss <marvin@xsteadfastx.org>
2023-03-11 09:04:36 +0000

    feat(vim,go): smaller spaces

--- .config/nvim/lua/xsfx/augroups/golang.lua
+++ .config/nvim/lua/xsfx/augroups/golang.lua
@@ -1,3 +1,3 @@
 require("xsfx.helpers").create_augroups({
-	go = { { "FileType", { pattern = "go", command = "setlocal noexpandtab tabstop=8 shiftwidth=8 softtabstop=8" } } },
+	go = { { "FileType", { pattern = "go", command = "setlocal noexpandtab tabstop=4 shiftwidth=4 softtabstop=4" } } },
 })
