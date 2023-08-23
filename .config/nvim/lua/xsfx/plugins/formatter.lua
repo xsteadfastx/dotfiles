f0c2ae69c7274af0bdaa42bfe04268b94e162233
Marvin Preuss <marvin@xsteadfastx.org>
2023-08-23 09:50:06 +0000

    feat(vim,formatter): remove single quotes

--- .config/nvim/lua/xsfx/plugins/formatter.lua
+++ .config/nvim/lua/xsfx/plugins/formatter.lua
@@ -84,7 +84,6 @@ return {
 								args = {
 									"--stdin-filepath",
 									vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
-									"--single-quote",
 								},
 								stdin = true,
 							}
