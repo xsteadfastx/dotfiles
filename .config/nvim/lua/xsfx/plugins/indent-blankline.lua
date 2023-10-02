4bfcce08b57a03744522dfa237d945f06f417c77
Marvin Preuss <marvin@xsteadfastx.org>
2023-10-02 12:27:16 +0000

    chore(vim): bumps indent-blankline plugin

--- .config/nvim/lua/xsfx/plugins/indent-blankline.lua
+++ .config/nvim/lua/xsfx/plugins/indent-blankline.lua
@@ -3,12 +3,7 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
-			require("indent_blankline").setup({
-				show_end_of_file = true,
-				space_char_blankline = " ",
-				show_current_context = true,
-				show_current_context_start = true,
-			})
+			require("ibl").setup({})
 		end,
 	},
 }
