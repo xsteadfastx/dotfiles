2b4ea1d67155b8f7da403d6f44cd0292e51800b0
Marvin Preuss <marvin@xsteadfastx.org>
2023-10-11 09:22:38 +0000

    fix(vim): lektor augroup

--- .config/nvim/lua/xsfx/augroups/lektor.lua
+++ .config/nvim/lua/xsfx/augroups/lektor.lua
@@ -1,3 +1,8 @@
 require("xsfx.helpers").create_augroups({
-	lektor = { { "BufRead,BufNewFile", { pattern = "*.lr", command = "set filetype=markdown" } } },
+	lektor = {
+		{
+			{ "BufRead",        "BufNewFile" },
+			{ pattern = "*.lr", command = "set filetype=markdown" },
+		},
+	},
 })
