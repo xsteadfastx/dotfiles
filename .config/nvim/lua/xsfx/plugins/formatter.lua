02637e8bcf15de23029454fe9673810e19d52286
Marvin Preuss <marvin@xsteadfastx.org>
2024-07-18 11:49:48 +0200

    feat(vim): moves from formatter.nvim to conform

--- .config/nvim/lua/xsfx/plugins/formatter.lua
+++ .config/nvim/lua/xsfx/plugins/formatter.lua
@@ -2,99 +2,48 @@
 local api = vim.api
 local group = api.nvim_create_augroup("format", { clear = true })
 
-api.nvim_create_autocmd("BufWritePost", {
+api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
-	callback = function()
-		vim.cmd("FormatWrite")
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
 	end,
 	group = group,
 })
 
 return {
 	{
-		"mhartington/formatter.nvim",
+		"stevearc/conform.nvim",
 		config = function()
-			require("formatter").setup({
-				filetype = {
-					go = {
-						function()
-							return {
-								exe = "golines",
-								args = { "--base-formatter=gofumpt" },
-								stdin = true,
-							}
-						end,
-					},
-					proto = {
-						function()
-							return {
-								exe = "clang-format",
-								args = {
-									"-style='{BasedOnStyle: Google, IndentWidth: 4, AlignConsecutiveDeclarations: true, AlignConsecutiveAssignments: true, ColumnLimit: 0}'",
-								},
-								stdin = true,
-							}
-						end,
-					},
-					sql = {
-						function()
-							return {
-								exe = "sql-formatter",
-								args = { "-l", "postgresql" },
-								stdin = true,
-							}
-						end,
-					},
-					lua = {
-						require("formatter.filetypes.lua").stylua,
-					},
-					yaml = {
-						require("formatter.filetypes.yaml").yamlfmt,
-					},
-					taskfile = {
-						require("formatter.filetypes.yaml").prettier,
-					},
-					sh = {
-						require("formatter.filetypes.sh").shfmt,
-					},
-					markdown = {
-						function()
-							return {
-								exe = "prettier",
-								args = {
-									"--stdin-filepath",
-									vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
-									"--single-quote",
-								},
-								stdin = true,
-							}
-						end,
-					},
-					json = {
-						function()
-							return {
-								exe = "prettier",
-								args = {
-									"--stdin-filepath",
-									vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
-									"--single-quote",
-								},
-								stdin = true,
-							}
-						end,
-					},
-					javascript = {
-						function()
-							return {
-								exe = "prettier",
-								args = {
-									"--stdin-filepath",
-									vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
-								},
-								stdin = true,
-							}
-						end,
-					},
+			require("conform").formatters.clang_format = {
+				prepend_args = {
+					"-style={BasedOnStyle: Google, IndentWidth: 4, AlignConsecutiveDeclarations: true, AlignConsecutiveAssignments: true, ColumnLimit: 0}",
+				},
+			}
+
+			require("conform").formatters.golines = {
+				prepend_args = { "--base-formatter=gofumpt" },
+			}
+
+			require("conform").formatters.sql_formatter = {
+				prepend_args = { "-l", "postgresql" },
+			}
+
+			require("conform").setup({
+				formatters_by_ft = {
+					go = { "golines" },
+					js = { "prettier" },
+					lua = { "stylua" },
+					markdown = { "prettier" },
+					proto = { "clang_format" },
+					sh = { "shfmt" },
+					sql = { "sql_formatter" },
+					taskfile = { "prettier" },
+					yaml = { "yamlfmt" },
+				},
+
+				format_on_save = {
+					lsp_format = "fallback",
+					timeout_ms = 500,
 				},
 			})
 		end,
