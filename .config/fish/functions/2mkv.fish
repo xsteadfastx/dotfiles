b53744ae9a2fdda2a3aa32c6138ff3648e01b8e5
Marvin Preuss <marvin@xsteadfastx.org>
2024-10-02 19:09:17 +0200

    feat(fish): 2mkv with some changes

--- .config/fish/functions/2mkv.fish
+++ .config/fish/functions/2mkv.fish
@@ -1,14 +1,28 @@
 function 2mkv
-    /usr/bin/HandBrakeCLI \
-        --input $argv[1] \
-        --output $argv[2] \
-        --main-feature --markers --optimize --ipod-atom \
-        --encoder-tune film \
-        --encoder x264 --encoder-profile high --encoder-preset medium --encoder-level 4.1 --quality 21 \
-        --maxWidth 1920 \
-        --maxHeight 1080 \
-        --decomb --auto-anamorphic --cfr \
-        --audio "1,2,3,4,5,6,7,8,9,10" \
-        --aencoder copy --audio-fallback av_aac --ab 160 \
-        --subtitle "1,2,3,4,5,6,7,8,9,10"
+	HandBrakeCLI \
+		--input $argv[1] \
+		--output $argv[2] \
+		--main-feature --markers --optimize --ipod-atom \
+		--encoder-tune film \
+		--encoder x264 --encoder-profile high --encoder-preset medium --encoder-level 4.1 --quality 20 \
+		--maxWidth 1920 \
+		--maxHeight 1080 \
+		--decomb --auto-anamorphic --cfr \
+		--all-audio \
+		--aencoder copy --audio-fallback av_aac --ab 160 \
+		--all-subtitles
+end
+
+# should test this with quality 18 and check filesize
+function 2mkv265
+	HandBrakeCLI \
+		--input $argv[1] \
+		--output $argv[2] \
+		--encoder x265 \
+		--encoder-preset medium \
+		--quality 18 \
+		--decomb --auto-anamorphic --cfr \
+		--all-audio \
+		--aencoder copy --audio-fallback av_aac --ab 160 \
+		--all-subtitles
 end
