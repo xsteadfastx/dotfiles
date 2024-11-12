ff436105abe7ce83ceb65e86b3ab0ceedecfdd8e
Marvin Preuss <marvin@xsteadfastx.org>
2024-11-12 09:26:10 +0100

    feat(vim): ignore .direnv

--- .config/nvim/lua/xsfx/plugins/fzf.lua
+++ .config/nvim/lua/xsfx/plugins/fzf.lua
@@ -11,14 +11,14 @@ return {
 		},
 		keys = {
 			{ "<Leader><space>", "<cmd>lua require('fzf-lua').buffers()<CR>" },
-			{ "<Leader>tt",      "<cmd>lua require('fzf-lua').tabs()<CR>" },
-			{ "<Leader>ff",      "<cmd>lua require('fzf-lua').files()<CR>" },
-			{ "<Leader>rg",      "<cmd>lua require('fzf-lua').grep_project()<CR>" },
-			{ "<Leader>ll",      "<cmd>lua require('fzf-lua').grep_curbuf()<CR>" },
-			{ "<Leader>cm",      "<cmd>lua require('fzf-lua').git_commits()<CR>" },
-			{ "<Leader>cf",      "<cmd>lua require('fzf-lua').git_bcommits()<CR>" },
-			{ "<Leader>:",       "<cmd>lua require('fzf-lua').commands()<CR>" },
-			{ "<Leader>fr",      "<cmd>lua require('fzf-lua').lsp_references()<CR>" },
+			{ "<Leader>tt", "<cmd>lua require('fzf-lua').tabs()<CR>" },
+			{ "<Leader>ff", "<cmd>lua require('fzf-lua').files()<CR>" },
+			{ "<Leader>rg", "<cmd>lua require('fzf-lua').grep_project()<CR>" },
+			{ "<Leader>ll", "<cmd>lua require('fzf-lua').grep_curbuf()<CR>" },
+			{ "<Leader>cm", "<cmd>lua require('fzf-lua').git_commits()<CR>" },
+			{ "<Leader>cf", "<cmd>lua require('fzf-lua').git_bcommits()<CR>" },
+			{ "<Leader>:", "<cmd>lua require('fzf-lua').commands()<CR>" },
+			{ "<Leader>fr", "<cmd>lua require('fzf-lua').lsp_references()<CR>" },
 		},
 		config = function()
 			require("fzf-lua").setup({
@@ -26,12 +26,10 @@ return {
 					fullscreen = true,
 				},
 				files = {
-					fd_opts =
-					"--color=always --type f --hidden --no-ignore --follow --exclude .git --exclude vendor --exclude .cache",
+					fd_opts = "--color=always --type f --hidden --no-ignore --follow --exclude .git --exclude vendor --exclude .cache --exclude .direnv",
 				},
 				grep = {
-					rg_opts =
-					"--no-ignore-vcs --hidden --column --line-number --no-heading --color=always --smart-case --max-columns=512 -g '!vendor/*' -g '!.git/*'",
+					rg_opts = "--no-ignore-vcs --hidden --column --line-number --no-heading --color=always --smart-case --max-columns=512 -g '!vendor/*' -g '!.git/*' -g '!.direnv/*'",
 				},
 			})
 		end,
