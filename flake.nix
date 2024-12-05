2df89cf9116e5781091edb409200cc8ed6b3af0a
Marvin Preuss <marvin@xsteadfastx.org>
2024-12-05 11:41:23 +0100

    refactor: some home-manager stuff#

--- flake.nix
+++ flake.nix
@@ -20,14 +20,82 @@
   outputs = inputs:
     let
       system = "x86_64-linux";
+
       pkgs = import inputs.nixpkgs { inherit system; };
+
+      pkgsUnstable = import inputs.nixpkgs-unstable {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+        overlays = [
+          # up to date neovim
+          (final: prev: {
+            neovim = prev.neovim-unwrapped.overrideAttrs {
+              src = prev.fetchFromGitHub {
+                owner = "neovim";
+                repo = "neovim";
+                rev = "v0.10.2";
+                hash = "sha256-+qjjelYMB3MyjaESfCaGoeBURUzSVh/50uxUqStxIfY=";
+              };
+            };
+          })
+
+          # needed because there is a system gpg-agent and gpg from wrapped gopass mismatch
+          (final: prev: {
+            gopass = prev.gopass.overrideAttrs rec {
+              wrapperPath = pkgs.lib.makeBinPath ([ prev.git prev.xclip ] ++ pkgs.lib.optional prev.stdenv.isLinux prev.wl-clipboard);
+              postFixup = ''
+                wrapProgram $out/bin/gopass \
+                  --prefix PATH : "${wrapperPath}" \
+                  --set GOPASS_NO_REMINDER true
+              '';
+            };
+          })
+
+          (final: prev: {
+            go-task = prev.go-task.overrideAttrs (finalAttrs: previousAttrs: { patches = [ ]; });
+          })
+
+          (final: prev: {
+            localsend-go = prev.buildGo123Module rec {
+              pname = "localsend-go";
+              version = "1.1.0";
+
+              src = prev.fetchFromGitHub {
+                owner = "meowrain";
+                repo = "localsend-go";
+                rev = version;
+                hash = "sha256-oQqK0Omqeqs0gflyDt72TbJUeNmCr3kzlFblgAwayYQ=";
+              };
+
+              vendorHash = "sha256-wjrtv1Y1umQlbc1JhYd4uJ9QXCtucAUI8WCbeIqE1do=";
+              subPackages = "cmd/";
+              ldflags = [
+                "-s"
+                "-w"
+              ];
+              CGO_ENABLED = 0;
+
+              postInstall = ''
+                mv $out/bin/cmd $out/bin/localsend-go
+              '';
+            };
+          })
+
+          (final: prev: {
+            airmtp = inputs.airmtp.packages.${system}.default;
+          })
+
+        ];
+      };
     in
     {
       homeConfigurations = {
         xsfx = inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [ ./home.nix ];
-          extraSpecialArgs = { inherit system inputs; };
+          extraSpecialArgs = { inherit system pkgsUnstable; };
         };
       };
     };
--- home.nix
+++ home.nix
@@ -1,78 +1,11 @@
-{ config, pkgs, system, inputs, ... }@args:
-
-let
-  pkgs-unstable = import inputs.nixpkgs-unstable {
-    inherit system;
-    config = {
-      allowUnfree = true;
-    };
-    overlays = [
-      # up to date neovim
-      (final: prev: {
-        neovim = prev.neovim-unwrapped.overrideAttrs {
-          src = prev.fetchFromGitHub {
-            owner = "neovim";
-            repo = "neovim";
-            rev = "v0.10.2";
-            hash = "sha256-+qjjelYMB3MyjaESfCaGoeBURUzSVh/50uxUqStxIfY=";
-          };
-        };
-      })
-
-      # needed because there is a system gpg-agent and gpg from wrapped gopass mismatch
-      (final: prev: {
-        gopass = prev.gopass.overrideAttrs rec {
-          wrapperPath = args.lib.makeBinPath ([ prev.git prev.xclip ] ++ args.lib.optional prev.stdenv.isLinux prev.wl-clipboard);
-          postFixup = ''
-            wrapProgram $out/bin/gopass \
-              --prefix PATH : "${wrapperPath}" \
-              --set GOPASS_NO_REMINDER true
-          '';
-        };
-      })
-
-      (final: prev: {
-        go-task = prev.go-task.overrideAttrs (finalAttrs: previousAttrs: { patches = [ ]; });
-      })
-
-      (final: prev: {
-        localsend-go = prev.buildGo123Module rec {
-          pname = "localsend-go";
-          version = "1.1.0";
-
-          src = prev.fetchFromGitHub {
-            owner = "meowrain";
-            repo = "localsend-go";
-            rev = version;
-            hash = "sha256-oQqK0Omqeqs0gflyDt72TbJUeNmCr3kzlFblgAwayYQ=";
-          };
-
-          vendorHash = "sha256-wjrtv1Y1umQlbc1JhYd4uJ9QXCtucAUI8WCbeIqE1do=";
-          subPackages = "cmd/";
-          ldflags = [
-            "-s"
-            "-w"
-          ];
-          CGO_ENABLED = 0;
-
-          postInstall = ''
-            mv $out/bin/cmd $out/bin/localsend-go
-          '';
-        };
-      })
-    ];
-  };
-
-  airmtp = inputs.airmtp.packages.${system}.default;
-in
-
+{ pkgsUnstable, ... }:
 {
   home.username = "marv";
   home.homeDirectory = "/home/marv";
 
   home.stateVersion = "24.05";
 
-  home.packages = with pkgs-unstable; [
+  home.packages = with pkgsUnstable; [
     # systemtools
     bat
     btop
