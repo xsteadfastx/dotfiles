c010d870e8ca80ba2c186b75a646ac20bca6ba87
Marvin Preuss <marvin@xsteadfastx.org>
2024-12-30 10:44:18 +0100

    feat: uses gnome and gdm

--- .nix-configurations/hosts/troy/configuration.nix
+++ .nix-configurations/hosts/troy/configuration.nix
@@ -39,8 +39,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.windowManager.i3.enable = true;
 
