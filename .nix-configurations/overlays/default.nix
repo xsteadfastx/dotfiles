73f4e610b3e186e6e2da1f756dda587557149217
Marvin Preuss <marvin@xsteadfastx.org>
2024-12-30 10:44:48 +0100

    feat: uses system neovim and gopass

--- .nix-configurations/overlays/default.nix
+++ .nix-configurations/overlays/default.nix
@@ -2,25 +2,25 @@
 final: prev: {
 
   # up to date neovim
-  neovim = prev.neovim-unwrapped.overrideAttrs {
-    src = prev.fetchFromGitHub {
-      owner = "neovim";
-      repo = "neovim";
-      rev = "v0.10.2";
-      hash = "sha256-+qjjelYMB3MyjaESfCaGoeBURUzSVh/50uxUqStxIfY=";
-    };
-  };
+  # neovim = prev.neovim-unwrapped.overrideAttrs {
+  #   src = prev.fetchFromGitHub {
+  #     owner = "neovim";
+  #     repo = "neovim";
+  #     rev = "v0.10.3";
+  #     hash = "sha256-nmnEyHE/HcrwK+CyJHNoLG0BqjnWleiBy0UYcJL7Ecc=";
+  #   };
+  # };
 
   # needed because there is a system gpg-agent and gpg from wrapped gopass mismatch
-  gopass = prev.gopass.overrideAttrs rec {
-    wrapperPath = pkgs.lib.makeBinPath ([ prev.git prev.xclip ]
-      ++ pkgs.lib.optional prev.stdenv.isLinux prev.wl-clipboard);
-    postFixup = ''
-      wrapProgram $out/bin/gopass \
-        --prefix PATH : "${wrapperPath}" \
-        --set GOPASS_NO_REMINDER true
-    '';
-  };
+  # gopass = prev.gopass.overrideAttrs rec {
+  #   wrapperPath = pkgs.lib.makeBinPath ([ prev.git prev.xclip ]
+  #     ++ pkgs.lib.optional prev.stdenv.isLinux prev.wl-clipboard);
+  #   postFixup = ''
+  #     wrapProgram $out/bin/gopass \
+  #       --prefix PATH : "${wrapperPath}" \
+  #       --set GOPASS_NO_REMINDER true
+  #   '';
+  # };
 
   go-task =
     prev.go-task.overrideAttrs (finalAttrs: previousAttrs: { patches = [ ]; });
