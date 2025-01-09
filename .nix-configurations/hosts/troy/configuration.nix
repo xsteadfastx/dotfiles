3c1fb0ab9260e9ee8b01d4240587d72e7ad954ff
Marvin Preuss <marvin@xsteadfastx.org>
2025-01-09 09:17:53 +0100

    feat: using i3lock-color dracula

--- .nix-configurations/hosts/troy/configuration.nix
+++ .nix-configurations/hosts/troy/configuration.nix
@@ -45,9 +45,65 @@
   services.xserver.windowManager.i3.enable = true;
 
   # locking screen
-  programs.xss-lock = {
+  programs.xss-lock = let
+    draculaLock = pkgs.writeShellScriptBin "dracula-lock" ''
+      alpha='dd'
+      background='#282a36'
+      selection='#44475a'
+      comment='#6272a4'
+
+      yellow='#f1fa8c'
+      orange='#ffb86c'
+      red='#ff5555'
+      magenta='#ff79c6'
+      blue='#6272a4'
+      cyan='#8be9fd'
+      green='50fa7b'
+
+      ${pkgs.i3lock-color}/bin/i3lock-color \
+        --insidever-color=$selection$alpha \
+        --insidewrong-color=$selection$alpha \
+        --inside-color=$selection$alpha \
+        --ringver-color=$green$alpha \
+        --ringwrong-color=$red$alpha \
+        --ringver-color=$green$alpha \
+        --ringwrong-color=$red$alpha \
+        --ring-color=$blue$alpha \
+        --line-uses-ring \
+        --keyhl-color=$magenta$alpha \
+        --bshl-color=$orange$alpha \
+        --separator-color=$selection$alpha \
+        --verif-color=$green \
+        --wrong-color=$red \
+        --modif-color=$red \
+        --layout-color=$blue \
+        --date-color=$blue \
+        --time-color=$blue \
+        --screen 1 \
+        --blur 1 \
+        --clock \
+        --indicator \
+        --time-str="%H:%M:%S" \
+        --date-str="%A %e %B %Y" \
+        --verif-text="Checking..." \
+        --wrong-text="Wrong pswd" \
+        --noinput="No Input" \
+        --lock-text="Locking..." \
+        --lockfailed="Lock Failed" \
+        --radius=120 \
+        --ring-width=10 \
+        --pass-media-keys \
+        --pass-screen-keys \
+        --pass-volume-keys \
+        --time-font="BlexMono Nerd Font" \
+        --date-font="BlexMono Nerd Font" \
+        --layout-font="BlexMono Nerd Font" \
+        --verif-font="BlexMono Nerd Font" \
+        --wrong-font="BlexMono Nerd Font"
+    '';
+  in {
     enable = true;
-    lockerCommand = "${pkgs.xsecurelock}/bin/xsecurelock";
+    lockerCommand = "${draculaLock}/bin/dracula-lock";
   };
 
   # Configure keymap in X11
