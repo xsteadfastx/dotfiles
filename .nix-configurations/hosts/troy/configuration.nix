3f6e984021384ccdfe6b2022a4a838b34e0f2ae5
Marvin Preuss <marvin@xsteadfastx.org>
2025-01-21 10:07:24 +0100

    feat: locking via xsecurelock

--- .nix-configurations/hosts/troy/configuration.nix
+++ .nix-configurations/hosts/troy/configuration.nix
@@ -56,64 +56,12 @@
 
   # locking screen
   programs.xss-lock = let
-    draculaLock = pkgs.writeShellScriptBin "dracula-lock" ''
-      alpha='dd'
-      background='#282a36'
-      selection='#44475a'
-      comment='#6272a4'
-
-      yellow='#f1fa8c'
-      orange='#ffb86c'
-      red='#ff5555'
-      magenta='#ff79c6'
-      blue='#6272a4'
-      cyan='#8be9fd'
-      green='50fa7b'
-
-      ${pkgs.i3lock-color}/bin/i3lock-color \
-        --insidever-color=$selection$alpha \
-        --insidewrong-color=$selection$alpha \
-        --inside-color=$selection$alpha \
-        --ringver-color=$green$alpha \
-        --ringwrong-color=$red$alpha \
-        --ringver-color=$green$alpha \
-        --ringwrong-color=$red$alpha \
-        --ring-color=$blue$alpha \
-        --line-uses-ring \
-        --keyhl-color=$magenta$alpha \
-        --bshl-color=$orange$alpha \
-        --separator-color=$selection$alpha \
-        --verif-color=$green \
-        --wrong-color=$red \
-        --modif-color=$red \
-        --layout-color=$blue \
-        --date-color=$blue \
-        --time-color=$blue \
-        --screen 1 \
-        --blur 1 \
-        --clock \
-        --indicator \
-        --time-str="%H:%M:%S" \
-        --date-str="%A %e %B %Y" \
-        --verif-text="Checking..." \
-        --wrong-text="Wrong pswd" \
-        --noinput="No Input" \
-        --lock-text="Locking..." \
-        --lockfailed="Lock Failed" \
-        --radius=120 \
-        --ring-width=10 \
-        --pass-media-keys \
-        --pass-screen-keys \
-        --pass-volume-keys \
-        --time-font="JetBrainsMono Nerd Font" \
-        --date-font="JetBrainsMono Nerd Font" \
-        --layout-font="JetBrainsMono Nerd Font" \
-        --verif-font="JetBrainsMono Nerd Font" \
-        --wrong-font="JetBrainsMono Nerd Font"
+    lock = pkgs.writeShellScriptBin "lock" ''
+      XSECURELOCK_PASSWORD_PROMPT=time_hex XSECURELOCK_FONT='JetBrainsMono Nerd Font' ${pkgs.xsecurelock}/bin/xsecurelock
     '';
   in {
     enable = true;
-    lockerCommand = "${draculaLock}/bin/dracula-lock";
+    lockerCommand = "${lock}/bin/lock";
   };
 
   # Configure keymap in X11
