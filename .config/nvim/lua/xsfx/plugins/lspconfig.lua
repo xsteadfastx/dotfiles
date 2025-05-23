8c1ba040190d967fc5bb90eefb9480bfc5cac95c
Marvin Preuss <marvin@xsteadfastx.org>
2025-05-23 09:46:39 +0200

    feat: prepare lsp virtualtext if wanted

--- .config/nvim/lua/xsfx/plugins/lspconfig.lua
+++ .config/nvim/lua/xsfx/plugins/lspconfig.lua
@@ -82,6 +82,8 @@ return {
 			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 				border = "single",
 			})
+
+			-- vim.diagnostic.config({ virtual_text = true })
 		end,
 	},
 }
