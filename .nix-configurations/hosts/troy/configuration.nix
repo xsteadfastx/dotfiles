3702a7394ed822755c47df931d654ce9189dd1fd
Marvin Preuss <marvin@xsteadfastx.org>
2025-05-23 14:33:03 +0200

    feat(troy): adds localsend

--- .nix-configurations/hosts/troy/configuration.nix
+++ .nix-configurations/hosts/troy/configuration.nix
@@ -224,6 +224,11 @@
   # Needs to be enabled for completions
   programs.fish.enable = true;
 
+  programs.localsend = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
